a7d1d2f85bfb87fd028d50d18120a6c6201cf888
g1gantino <37074696+g1gantino@users.noreply.github.com>
2020-10-20 14:15:12 +0200

    Add files via upload

---
+++ Set.h
@@ -0,0 +1,26 @@
+#include <Windows.h>
+#include <TlHelp32.h>
+#include "JunkCode.h"
+#include "iostream"
+#include <string>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+
+using namespace std;
+
+string dll_name;
+string find_dll;
+string title;
+string color;
+string echo_off;
+string cls;
+int x;
+int y;
+int z;
+int _x;
+int _y;
+int _z;
+int yeet;
+int yah;
+int yeah_motherfucking_yeet;
