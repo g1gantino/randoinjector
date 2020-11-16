296938e7419a97551b715c49bff12f8bdc7573e4
g1gantino <37074696+g1gantino@users.noreply.github.com>
2020-11-16 16:08:32 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,6 @@
 #include <TlHelp32.h>
 #include "iostream"
 #include <string>
-#include <chrono>
 #include <thread>
 #include <cstdlib>
 #include <ctime>
@@ -24,18 +23,7 @@ int _z;
 Injector is made by the creator of randoinjector, then fixed by martinjanas to fix with trustlaunch, then I added some misc stuff here.
 It is nothing special but it works, is UD and is free.
 Feel free to use it in your videos etc.
-==\
-Colors:
-(for you if you want to change colors)
-#define FOREGROUND_BLUE      0x0001 // text color contains blue.
-#define FOREGROUND_GREEN     0x0002 // text color contains green.
-#define FOREGROUND_RED       0x0004 // text color contains red.
-#define FOREGROUND_INTENSITY 0x0008 // text color is intensified.
-#define BACKGROUND_BLUE      0x0010 // background color contains blue.
-#define BACKGROUND_GREEN     0x0020 // background color contains green.
-#define BACKGROUND_RED       0x0040 // background color contains red.
-#define BACKGROUND_INTENSITY 0x0080 // background color is intensified.
-*/
+
 
 
 
